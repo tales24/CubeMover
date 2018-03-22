2970d8caf7dd771f32c4232c10e0f892f757c96f
QRi2 Glania <qri2@hotmail.de>
2018-03-22 17:15:33 +0100

    validation for Fileimporter & Flow Program

--- CubeSolver/src/de/inmediasp/nxt/Importer.java
+++ CubeSolver/src/de/inmediasp/nxt/Importer.java
@@ -6,7 +6,6 @@ package de.inmediasp.nxt;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,32 +15,43 @@ import java.util.List;
  *
  */
 public class Importer {
-	
-	public List<String> convertTxtFileToInput(String filePath){
+
+	public List<String> convertTxtFileToInput(String filePath) {
 		try {
 			return read(filePath);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
-	public List<String> read(String filePath) throws IOException {
+
+	public List<String> read(String filePath) throws Exception {
 		String line;
-		List<String> ausgabe = new ArrayList<String>(); 
-		
+		List<String> output = new ArrayList<String>();
+		char currentLetter;
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(filePath))));
-		try {			
+		try {
 			while ((line = br.readLine()) != null) {
-				for(int i = 0 ; i< line.length();i++) {
-					ausgabe.add(line.charAt(i)+"");
+				for (int i = 0; i < line.length(); i++) {
+					currentLetter = line.charAt(i);
+
+					if (currentLetter == 'R' || currentLetter == 'r' || currentLetter == 'L' || currentLetter == 'l'
+							|| currentLetter == 'U' || currentLetter == 'u' || currentLetter == 'D'
+							|| currentLetter == 'd' || currentLetter == 'F' || currentLetter == 'f'
+							|| currentLetter == 'B' || currentLetter == 'b' || currentLetter == 'X'
+							|| currentLetter == 'x' || currentLetter == 'Y' || currentLetter == 'y') {
+						output.add(currentLetter + "");
+					}
+					else {
+						throw new Exception("Ungueltige Rotationsfolge! " + currentLetter + " ist keine valide Rotationsrichtung");
+					}
 				}
 			}
 		} finally {
 			br.close();
 		}
-		return ausgabe;
-		
+		return output;
+
 	}
 }
--- CubeSolver/src/de/inmediasp/nxt/Main.java
+++ CubeSolver/src/de/inmediasp/nxt/Main.java
@@ -1,25 +1,86 @@
 package de.inmediasp.nxt;
 
+import java.util.List;
+
 import lejos.nxt.MotorPort;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
-import lejos.util.Delay;
 
 public class Main {
 	public static void main(String[] args) {		
 		
-//		Importer importer = new Importer();		
-//		importer.convertTxtFileToInput("C:\\Users\\qri2\\Desktop\\Testdukoment.txt");
 		
 		final PlatformMover platformMover = new PlatformMover(new NXTRegulatedMotor(MotorPort.A));
 		final ArmMover armMover = new ArmMover(new NXTRegulatedMotor(MotorPort.C), new UltrasonicSensor(SensorPort.S1));
 
 		final MoveController moveController = new MoveController(armMover, platformMover);
 		
-		moveController.reset();
-		moveController.movement_f();
-		Delay.msDelay(1000);
-		moveController.movement_F();
+		Importer importer = new Importer();		
+		List<String> rotations = importer.convertTxtFileToInput("C:\\Users\\qri2\\Desktop\\Testdukoment.txt");
+		
+		for (String rotation : rotations) {
+			
+			switch (rotation) {
+			case "R":
+				moveController.movement_R();
+				break;
+			case "r":
+				moveController.movement_r();
+				break;
+			case "L":
+				moveController.movement_L();
+				break;
+			case "l":
+				moveController.movement_l();
+				break;
+			case "U":
+				moveController.movement_U();
+				break;
+			case "u":
+				moveController.movement_u();
+				break;
+			case "D":
+				moveController.movement_D();
+				break;
+			case "d":
+				moveController.movement_d();
+				break;
+			case "F":
+				moveController.movement_F();
+				break;
+			case "f":
+				moveController.movement_f();
+				break;
+			case "B":
+				moveController.movement_B();
+				break;
+			case "b":
+				moveController.movement_b();
+				break;
+			case "X":
+				moveController.movement_X();
+				break;
+			case "x":
+				moveController.movement_x();
+				break;
+			case "Y":
+				moveController.movement_Y();
+				break;
+			case "y":
+				moveController.movement_y();
+				break;			
+
+			default:
+				break;
+			}
+			
+			
+		}
+		
+//		moveController.reset();
+//		moveController.movement_f();
+//		Delay.msDelay(1000);
+//		moveController.movement_F();
  	}
 }
